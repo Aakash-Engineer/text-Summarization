288087afe1a6d4e5b26b394105f3eb33a4060758
AakashEngineer <aakashpal1183@gmail.com>
2024-09-15 01:53:59 +0530

    Data transformation added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from textSummarizer.pipeline.step1_data_ingestion import DataIngestionTrainingPipeline
 from textSummarizer.logging import logger
+from textSummarizer.pipeline.step1_data_ingestion import DataIngestionTrainingPipeline
 from textSummarizer.pipeline.step2_data_validation import DataValidationTrainingPipeline
+from textSummarizer.pipeline.step3_data_transformation import DataTransformationTrainingPipeline
 
 try:
     logger.info("Starting data ingestion pipeline")
@@ -15,5 +16,13 @@ try:
     data_validation = DataValidationTrainingPipeline()
     data_validation.main()
     logger.info('Step 2 data validation completed')
+except Exception as e:
+    print(e)
+
+try:
+    logger.info("Step  data transformation pipeline")
+    data_transformation = DataTransformationTrainingPipeline()
+    data_transformation.main()
+    logger.info('Step 3 data transformation completed')
 except Exception as e:
     print(e)
--- src/textSummarizer/config/configuration.py
+++ src/textSummarizer/config/configuration.py
@@ -1,6 +1,6 @@
 from textSummarizer.constants import CONFIG_FILE_PATH, PARAMS_FILE_PATH
 from textSummarizer.utils.common import read_yaml, create_dirs
-from textSummarizer.entity import DataIngestionConfig, DataValidationConfig
+from textSummarizer.entity import DataIngestionConfig, DataValidationConfig, DataTransformationConfig
 class ConfigurationManager:
     def __init__(
             self, 
@@ -31,4 +31,13 @@ class ConfigurationManager:
             root_dir=config.root_dir,
             STATUS_FILE=config.STATUS_FILE,
             ALL_REQUIRED_FILES=config.ALL_REQUIRED_FILES
+        )
+    def get_data_transformation_config(self) -> DataTransformationConfig:
+        config = self.config.data_transformation
+        create_dirs([config.root_dir])
+
+        return DataTransformationConfig(
+            root_dir=config.root_dir,
+            data_path=config.data_path,
+            tokenizer_name=config.tokenizer_name
         )
